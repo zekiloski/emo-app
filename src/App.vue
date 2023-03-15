83663f2dadaed9b9330454c70277a4f2679b6752
Ezequiel <melli_ezequiel@hotmail.com>
2023-03-15 15:28:25 -0300

    Primer Render

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
-<!--Logica-->
 <script setup>
-const name = 'APP EMO';
-const stylecolor = "color: greenyellow";
-const arraycolores = ["blue","red","peru"];
-const activo = false;
+import Buttons from './components/Buttons.vue';
 </script>
 
-<!--Estructura render-->
 <template>
-  <h1>Bienvenidos a la {{ name.toLocaleUpperCase() }}</h1>
-  <p></p>
 
-  <h2 :style= "`color:${arraycolores[2]} `" >Fabrica de Implementos Agricolas</h2>
-  <h2>
-    {{ activo ? "Estoy activo" : "Estoy inactivo" }}
-  </h2>
+<div class="container">
+    <h1>APP EMO EN PRODUCCION</h1>
+    <p></p>
+    <Buttons/>
+</div>
+     
+  
 </template>
-
-
-<!--Estilos-->
-<style>
-  h1 {
-    color: green;
-  }
-</style>
---
+++ src/components/Buttons.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+    <button type="button" class="btn btn-outline-success btn-lg ">Prueba</button>
+</template>
+
+<style>
+
+</style>
+
